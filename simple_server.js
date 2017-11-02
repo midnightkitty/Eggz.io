f5f0386f78daeede662093367f53cc7814215db4
Chris Sims <csims314@gmail.com>
2017-11-02 12:10:35 -0500

    arrow keys hide open chat window, egg ninja belt images added

--- simple_server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(9000);
-console.log('listening on port 9000');
